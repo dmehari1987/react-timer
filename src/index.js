641faf0ee616c0aec1e32749405f0eafcb40dfeb
Dani <meharidaniel1111@gmail.com>
2020-04-20 15:50:48 +0200

    timer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Clock extends Component {
+    constructor (props){
+        super(props); 
+            this.state = {date: new Date()}
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000)
+    }
+
+    // componentWillUnmount() {
+    //     clearInterval(this.timerID)
+    // }
+
+    tick() {
+        this.setState({ date: new Date()})
+    }
+
+    render() {  
+        return ( 
+            <div>
+                <h1>Timing</h1>
+                <h2>The time is, {this.state.date.toLocaleTimeString()} </h2>
+            </div>
+         );
+    }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('root'))
+ 
+
+    ReactDOM.render( <Clock date= {new Date()} />, document.getElementById('root'))
+
